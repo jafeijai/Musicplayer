4579dbc3b5e86b5231f873c9b94df14d3395aee1
jytlee <jafeijai@gmail.com>
2018-09-19 23:59:44 +0900

    fix permissions

--- app/src/main/java/com/example/jytlee/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/jytlee/musicplayer/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.jytlee.musicplayer;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.MenuItem;
@@ -36,6 +39,12 @@ public class MainActivity extends Activity implements MediaPlayerControl {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
 
         songView = findViewById(R.id.song_list);
         songList = new ArrayList<>();
