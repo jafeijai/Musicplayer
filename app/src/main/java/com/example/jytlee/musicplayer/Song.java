6f8a537a1d4347878498ffbedb354b8c0c701749
jeffrey.lee <jeffrey.lee@linecorp.com>
2018-09-19 15:53:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jytlee/musicplayer/Song.java
@@ -0,0 +1,12 @@
+package com.example.jytlee.musicplayer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+}
